cf816eb1369d7ca6a42a9b10ad6271924d91b476
VitiosusMagnus <furkancanokal1@gmail.com>
2023-11-22 16:42:26 +0300

    bug fixes

--- src/main/java/com/vitiosusmagnus/demorestapi/TestController.java
+++ src/main/java/com/vitiosusmagnus/demorestapi/TestController.java
@@ -1,6 +1,5 @@
 package com.vitiosusmagnus.demorestapi;
 
-import jdk.incubator.vector.VectorOperators;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
