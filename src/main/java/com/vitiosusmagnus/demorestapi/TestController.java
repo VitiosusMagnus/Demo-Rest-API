e7c10ae8a6801763480892762678c97fabd97fee
VitiosusMagnus <furkancanokal1@gmail.com>
2023-11-23 18:05:53 +0300

    HomePage changed

--- src/main/java/com/vitiosusmagnus/demorestapi/TestController.java
+++ src/main/java/com/vitiosusmagnus/demorestapi/TestController.java
@@ -1,26 +1,16 @@
 package com.vitiosusmagnus.demorestapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @Autowired
-    TestRepository repo;
+
 
     @GetMapping("/")
     public String welcome(){
-        TestEntity person = new TestEntity();
-        person.setId(Long.valueOf(1));
-        person.setName("furkan");
-        repo.save(person);
-        String name = repo.findById(Long.valueOf(1)).orElseThrow().getName();
-
-        return  "<h1>" +
-                 name +
-                "Beyzayi Cok Seviyor" +
-                "<h1>";
+        return "index";
     }
 }
--- src/main/java/com/vitiosusmagnus/demorestapi/TestRepository.java
+++
@@ -1,6 +0,0 @@
-package com.vitiosusmagnus.demorestapi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity,Long> {
-}
